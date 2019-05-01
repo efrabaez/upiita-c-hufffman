f6a3183cbac4a295cf2bca4a2b906973c701a3db
Efraín Germán Báez Rubio <efrain@hyperlogics.com>
2019-05-01 00:39:05 -0500

    Frequency list table function done

--- Huffman.c
+++ Huffman.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h> //Librería que utilizo para cambiar todo a mayúscula.
+#include <string.h>
+#define MAX_LIMIT 50
 
 
  /*Definimos estructura*/
@@ -10,10 +11,25 @@ typedef struct _Node {
   struct _Node *left;
   struct _Node *right;
 } Node;
+
 /*Declaramos prototipo de las funciones*/
-
 void menu();
+/*Funciones guardar información fila*/
+Node * allocateMemory(char, int);
+Node * pushFirst(Node *, char, int);
+Node * pushLast(Node *, char, int);
+Node * pushPosition(Node *, char, int, int);
+Node * pushItem(Node *, char, int, int);
+/*Funcion tamaño lista*/
+int theSize(Node *);
+/*Funcion buscar elemento*/
+int searchItem(Node *, int);
 
+void showAll(Node *);
+
+/*Funciones del c{odigo de Huffman*/
+
+Node * frequencyTable(char *);
 
 int main(){
   menu();
@@ -22,5 +38,171 @@ int main(){
 }
 
 void menu(){
+  char sentence[MAX_LIMIT];
+  int opt = 0, i=0;
+
+  do{
+    printf("1.- Encode\n2.- Show encode\n3.- Decode\n4.- Show decode\n(-1).- Exit");
+    printf("\nType your choice: ");
+    scanf("%i", &opt);
+    fflush( stdin );
+
+
+    switch (opt) {
+      case 1:
+        printf("Type sentence to encode:");     
+        scanf ("%[^\n]%*c", sentence);
+        strupr(sentence);
+        printf("The string in upper case: %s\n", sentence);
+        frequencyTable(sentence);
+
+      break;
+      case 2:
+        printf("Type sentence to encode: ");     
+      break;
+
+    }
   
+   system("PAUSE");
+  } while(opt != -1);
+
 }
+
+
+/*Aquí comienzan las funciones del programa*/
+
+/*Inicio bloque insertar datos*/
+Node *allocateMemory(char _letter, int _frequency){
+	Node *newNode = NULL;
+	newNode = (Node *) malloc (sizeof(Node));
+	newNode->letter = _letter;
+  newNode->frequency = _frequency;
+	newNode->left = NULL;
+  newNode->right = NULL;
+
+	return newNode;
+}
+
+Node * pushFirst(Node *_list, char _letter, int _frequency){
+  Node *newNode = NULL;
+  newNode = allocateMemory(_letter, _frequency);
+  if(_list){
+    newNode->right = _list;
+    _list->left = newNode;
+    }
+  return newNode;
+}
+
+Node * pushLast(Node *_list, char _letter, int _frequency){
+  Node *newNode = NULL, *listAux = NULL;
+  newNode = allocateMemory(_letter, _frequency);
+  if (!_list){
+    _list = newNode;
+  }
+  else{
+    listAux = _list;
+    while(listAux->right){
+      listAux=listAux->right;
+    }
+    listAux->right = newNode;
+    newNode->left = listAux;
+  }
+  return _list;
+}
+
+Node * pushPosition(Node *_list, char _letter, int _frequency, int _position){
+  Node * newNode = NULL, *listAux = NULL;
+  int listSize = 0, i = 0;
+  listSize = theSize(_list);
+  if (listSize){
+    if (_position <= 0){
+      _list = pushFirst(_list, _letter, _frequency);
+    }else if (_position >= listSize){
+      _list =  pushLast(_list, _letter, _frequency);
+    }else{
+      listAux = _list;
+      newNode = allocateMemory(_letter, _frequency);
+      for(i=1; i < _position; i++){
+        printf("%i", i);
+        listAux = listAux->right;
+      }
+      newNode->right = listAux->right;
+      listAux->right->left = newNode;
+      listAux->right = newNode;
+      newNode->right = listAux;
+    }  
+  }else{
+    _list =  pushFirst(_list, _letter, _frequency);
+  }
+  return _list;
+}
+
+Node * pushItem(Node *_list, char _letter, int _frequency, int _item){
+  int position = 0;
+  position = searchItem(_list, _item);
+  _list = pushPosition(_list, _letter, _frequency, position);
+  return _list;
+}
+
+
+/*Funciones auxiliares*/
+int searchItem(Node * _list, int _data){
+  int position = 0;
+  while(_list){
+    if (_list->letter == _data) {
+      break;
+    }
+    _list = _list->right;
+    position++;
+  }
+  return position;
+}
+
+
+int theSize(Node *_list){
+  int listSize = 0;
+  while(_list){
+    listSize++;
+    _list=_list->right;
+  }
+  return listSize;
+}
+
+void showAll(Node *_list){
+  if (!_list){
+    printf("Vacia");
+  }
+  while(_list){
+    printf("Letra: %c\t Frecuencua: %i\n", _list->letter, _list->frequency);
+    _list = _list->right;
+  } 
+}
+
+/*Inicio bloque de Huffman*/
+
+Node * frequencyTable(char *_sentence){
+  Node *newNode = NULL, *listAux = NULL;
+  int i = 0, a=0;
+    while (_sentence[i] != '\0') {
+      if(newNode == NULL){
+        newNode = pushFirst(newNode, _sentence[i], 1);
+      }else {    
+        listAux = newNode;
+        while(listAux->right) {
+          if (listAux->letter == _sentence[i]) {
+            listAux->frequency++;
+            break;
+          }
+          listAux=listAux->right;
+         }
+         if (!listAux->right) {
+           newNode = pushLast(newNode, _sentence[i], 1);
+         }
+      }
+      i++;
+    }
+  showAll(newNode);
+  return newNode;
+}
+
+
